5543ade43e525ff9e05119fc63da11c25b98982f
reza elahi <relahi1373@gmail.com>
2023-02-19 15:12:57 +0330

    Dev: finalized php script

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # tutorials-php-design-patterns
+
+## Documentation
+
+How to run?
+
+```bash
+  cd tutorials-php-design-patterns
+  php index.php Behavioral ChainOfResponsibility
+```
--- index.php
+++ index.php
@@ -3,106 +3,14 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
-#Creational
-{
-    // ============= Simple Factory ===========//
+$design_category = $argv[1];
+$design_pattern = $argv[2];
 
-    {
-        // include __DIR__ . '/src/Creational/SimpleFactory/use-case.php';
-    }
+if (!file_exists(__DIR__ . "/src/{$design_category}/{$design_pattern}/use-case.php")) {
 
-    // ============= Factory Method ===========//
+    echo 'Design Pattern was not exists!';
 
-    {
-        // include __DIR__ . '/src/Creational/FactoryMethod/use-case.php';
-    }
-
-    // ============= Abstract Factory ===========//
-
-    {
-        // include __DIR__ . '/src/Creational/AbstractFactory/use-case.php';
-    }
-
-    // ============= Builder ===========//
-
-    {
-        // include __DIR__ . '/src/Creational/Builder/use-case.php';
-    }
-
-    // ============= Singleton ===========//
-
-    {
-        // include __DIR__ . '/src/Creational/Singleton/use-case.php';
-    }
+    die();
 }
 
-#Structural
-{
-    // ============= Adapter ===========//
-
-    // include __DIR__ . '/src/Structural/Adapter/use-case.php';
-
-    // ============= Bridge ===========//
-
-    // include __DIR__ . '/src/Structural/Bridge/use-case.php';
-
-    // ============= Composite ===========//
-
-    // include __DIR__ . '/src/Structural/Composite/use-case.php';
-
-    // ============= Decorator ===========//
-
-    // include __DIR__ . '/src/Structural/Decorator/use-case.php';
-
-    // ============= Facade ===========//
-
-    // include __DIR__ . '/src/Structural/Facade/use-case.php';
-
-    // ============= FlyWeight ===========//
-
-    // include __DIR__ . '/src/Structural/FlyWeight/use-case.php';
-
-    // ============= Proxy ===========//
-
-    // include __DIR__ . '/src/Structural/Proxy/use-case.php';
-}
-
-#Behavioral
-{
-    // ============= Chain of Responsibility ===========//
-
-    // include __DIR__ . '/src/Behavioral/ChainOfResponsibility/use-case.php';
-
-
-    // ============= Command ===========//
-
-    // include __DIR__ . '/src/Behavioral/Command/use-case.php';
-
-    // ============= Iterator ===========//
-
-    // include __DIR__ . '/src/Behavioral/Iterator/use-case.php';
-
-    // ============= Mediator ===========//
-
-    // include __DIR__ . '/src/Behavioral/Mediator/use-case.php';
-
-    // ============= Memento ===========//
-
-    // include __DIR__ . '/src/Behavioral/Memento/use-case.php';
-
-    // ============= Observer ===========//
-
-    // include __DIR__ . '/src/Behavioral/Observer/use-case.php';
-
-    // ============= Visitor ===========//
-
-    // include __DIR__ . '/src/Behavioral/Visitor/use-case.php';
-
-    // ============= Strategy ===========//
-
-    // include __DIR__ . '/src/Behavioral/Strategy/use-case.php';
-
-    // ============= Template Method ===========//
-
-    include __DIR__ . '/src/Behavioral/TemplateMethod/use-case.php';
-}
+include __DIR__ . "/src/{$design_category}/{$design_pattern}/use-case.php";
